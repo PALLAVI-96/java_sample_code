8f7ccbf68886587cf0b4228af84a776bfc68be1e
PALLAVI-96 <pallavisuresh96@gmail.com>
2025-06-22 14:28:31 +0530

    New Java code added for addition

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition{
+    public static void main(String[] args) {
+        int num_1=9;
+        int num_2=10;
+        int sum= num_1 +num_2;
+        System.out.println("sum of num_1 and num_2 is : " + sum);
+    }
+}
